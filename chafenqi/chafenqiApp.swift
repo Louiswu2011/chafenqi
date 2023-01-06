524fc2211c7434ea5cd02e93e8f005b68d3f9c94
Wu Louis <louiswu148@gmail.com>
2023-01-06 17:21:24 +0800

    Initial Commit

---
+++ chafenqi/chafenqiApp.swift
@@ -0,0 +1,17 @@
+//
+//  chafenqiApp.swift
+//  chafenqi
+//
+//  Created by 刘易斯 on 2023/1/6.
+//
+
+import SwiftUI
+
+@main
+struct chafenqiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ chafenqiUITests/chafenqiUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  chafenqiUITestsLaunchTests.swift
+//  chafenqiUITests
+//
+//  Created by 刘易斯 on 2023/1/6.
+//
+
+import XCTest
+
+final class chafenqiUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
