48f16219441c95630a262a8cb9a31aaa4b56bb80
Wu Louis <louiswu148@gmail.com>
2024-06-06 23:33:31 +0900

    fixed crash when loading utage charts

--- chafenqi/View/v2/Custom/LevelBlockView.swift
+++ chafenqi/View/v2/Custom/LevelBlockView.swift
@@ -15,12 +15,10 @@ struct LevelStripView: View {
         let levelColor = mode == 0 ? chunithmLevelColor : maimaiLevelColor
         
         HStack {
-            LevelBlockView(color: levelColor[0]!, level: levels[0])
-            LevelBlockView(color: levelColor[1]!, level: levels[1])
-            LevelBlockView(color: levelColor[2]!, level: levels[2])
-            LevelBlockView(color: levelColor[3]!, level: levels[3])
-            if (levels.count > 4 && levels[4] != "0") {
-                LevelBlockView(color: levelColor[4]!, level: levels[4])
+            ForEach(levels.indices, id: \.self) { index in
+                if let color = levelColor[index], levels[index] != "0" {
+                    LevelBlockView(color: color, level: levels[index])
+                }
             }
         }
     }
