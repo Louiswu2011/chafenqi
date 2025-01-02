faa3b9129bac37e6d97a98b83a01558b1d0f321f
louiswu2011 <louiswu148@gmail.com>
2025-01-02 19:35:34 +0900

    Add rotate team code and disband funtionality

--- chafenqi/View/v2/Team/Setting/TeamSettingsPage.swift
+++ chafenqi/View/v2/Team/Setting/TeamSettingsPage.swift
@@ -8,19 +8,23 @@
 import Foundation
 import SwiftUI
 import Inject
+import AlertToast
 
 struct TeamSettingsPage: View {
+    @Environment(\.dismiss) var dismiss
     @ObserveInjection var inject
     
     @ObservedObject var team: CFQTeam
     @ObservedObject var user: CFQNUser
+    @ObservedObject var alertToastModel = AlertToastModel.shared
     
     @State var promotable: Bool = false
     
     @State var courseName: String = ""
     @State var courseHealth: Int = 0
     
-    
+    @State var showRotateTeamCodeConfirmAlert: Bool = false
+    @State var showDisbandTeamConfirmAlert: Bool = false
     
     var body: some View {
         let promotableToggle = Binding {
@@ -90,12 +94,12 @@ struct TeamSettingsPage: View {
             
             Section {
                 Button {
-                    
+                    showRotateTeamCodeConfirmAlert.toggle()
                 } label: {
                     Text("重新生成团队代码...")
                 }
                 Button {
-                    
+                    showDisbandTeamConfirmAlert.toggle()
                 } label: {
                     Text("解散...")
                         .foregroundStyle(Color.red)
@@ -110,9 +114,50 @@ struct TeamSettingsPage: View {
         }
         .navigationTitle("团队设置")
         .navigationBarTitleDisplayMode(.inline)
+        .alert("更新团队代码", isPresented: $showRotateTeamCodeConfirmAlert) {
+            Button("取消", role: .cancel) {}
+            Button("确定") {
+                onRotateTeamCode()
+            }
+        } message: {
+            Text("确认要更新团队代码吗？更新后将无法通过原有的团队代码加入该团队。")
+        }
+        .alert("解散团队", isPresented: $showDisbandTeamConfirmAlert) {
+            Button("取消", role: .cancel) {}
+            Button("确定", role:. destructive) {
+                onDisbandTeam()
+            }
+        } message: {
+            Text("确认要解散团队吗？该操作无法撤销。")
+        }
     }
     
     func updatePromotable(newValue: Bool) async -> Bool {
         return await CFQTeamServer.adminUpdateTeamPromotable(authToken: user.jwtToken, game: user.currentMode, teamId: team.current.info.id, promotable: newValue)
+    }
+    
+    func onRotateTeamCode() {
+        Task {
+            let result = await CFQTeamServer.adminRotateTeamCode(authToken: user.jwtToken, game: user.currentMode, teamId: team.current.info.id)
+            if result {
+                alertToastModel.toast = AlertToast(displayMode: .hud, type: .complete(.green), title: "更新团队代码成功", subTitle: "请返回团队信息页查看")
+                team.refresh(user: user)
+            } else {
+                alertToastModel.toast = AlertToast(displayMode: .hud, type: .error(.red), title: "更新团队代码失败", subTitle: "未知错误，请联系开发者")
+            }
+        }
+    }
+    
+    func onDisbandTeam() {
+        Task {
+            let result = await CFQTeamServer.adminDisbandTeam(authToken: user.jwtToken, game: user.currentMode, teamId: team.current.info.id)
+            if result {
+                alertToastModel.toast = AlertToast(displayMode: .hud, type: .complete(.green), title: "解散成功")
+                team.refresh(user: user)
+                dismiss()
+            } else {
+                alertToastModel.toast = AlertToast(displayMode: .hud, type: .error(.red), title: "解散失败", subTitle: "未知错误，请联系开发者")
+            }
+        }
     }
 }
